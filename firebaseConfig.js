430638fc63b26af387feab00f20b5ed39a7b5fea
salemaziel <mymainemail0501@gmail.com>
2020-07-30 17:17:30 -0700

    update

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -17,15 +17,3 @@ module.exports = {
 
 
 
-/*module.exports = {
-  apiKey: 'AIzaSyByN1o4Zl6033C0CllxBMx1ltArBvaU66E',
-  authDomain: 'elitecarechiropractic-9b4f3.firebaseapp.com',
-  databaseURL: 'https://elitecarechiropractic-9b4f3.firebaseio.com',
-  projectId: 'elitecarechiropractic-9b4f3',
-  storageBucket: 'elitecarechiropractic-9b4f3.appspot.com',
-  messagingSenderId: '795647646039',
-  appId: '1:795647646039:web:95640bc052d1ffc9e84612',
-};
-*/
-
-
