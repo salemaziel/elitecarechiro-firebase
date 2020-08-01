3ed88e3691be137eb1abbf52e26fc53a85d7db54
salemaziel <mymainemail0501@gmail.com>
2020-07-31 17:27:03 -0700

    added gatsby_firebase to env

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,8 @@
 module.exports = {
-  apiKey: `process.env.GATSBY_API_KEY`,
-  authDomain: `process.env.GATSBY_AUTH_DOMAIN`,
-  databaseURL: `process.env.GATSBY_DATABASE_URL`,
-  projectId: `process.env.GATSBY_PROJECT_ID`,
-  storageBucket: `process.env.GATSBY_STORAGE_BUCKET`,
-  messagingSenderId: `process.env.GATSBY_MESSAGING_SENDER_ID`,
-  appId: `process.env.GATSBY_APP_ID`
+  apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+  authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
 };
