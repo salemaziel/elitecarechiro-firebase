87240ef5bfbd689869258026b0b9e459c78e642a
salemaziel <mymainemail0501@gmail.com>
2020-09-09 03:42:58 -0700

    update

--- src/components/CalendlyButton.js
+++ src/components/CalendlyButton.js
@@ -7,7 +7,7 @@ import calendlystyle from './nav.module.css'
 //const CalendlyButton = ({ url, prefill, pageSettings, utm }) => {
 //  const onClick = () => openPopupWidget({ url, prefill, pageSettings, utm });
 const CalendlyButton = ({ url }) => {
-    const onClick = () => openPopupWidget({ url:"https://calendly.com/elite-care-chiropractic/30min-appointment" });
+    const onClick = () => openPopupWidget({ url:"https://calendly.com/elite-care-chiropractic" });
   return <Button onClick={onClick} /*className={calendlystyle.NavBook}*/ className="blue-btns btn btn-primary">Book Online</Button>
 }
 
